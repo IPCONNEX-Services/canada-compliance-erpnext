e669e89831da6d53f4141a776bd0820682145baf
Omar <oelmohri@ipconnex.com>
2026-04-21 20:55:40 +0100

    feat: add use_tax_rules toggle to CA Tax Settings

--- canada_business_compliance/ca_sales_tax/doctype/ca_tax_settings/ca_tax_settings.py
+++ canada_business_compliance/ca_sales_tax/doctype/ca_tax_settings/ca_tax_settings.py
@@ -3,4 +3,11 @@ from frappe.model.document import Document
 
 
 class CATaxSettings(Document):
-    pass
+    def on_update(self):
+        disabled = 0 if self.use_tax_rules else 1
+        rules = frappe.get_all(
+            "Tax Rule",
+            filters={"tax_type": "Sales", "name": ["like", "CA %"]},
+        )
+        for rule in rules:
+            frappe.db.set_value("Tax Rule", rule.name, "disabled", disabled)
